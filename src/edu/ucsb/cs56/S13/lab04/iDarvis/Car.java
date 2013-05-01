95a4e545190c08d900791c54a90e576337d5c51d
Daryl Pham <d_s_p@umail.ucsb.edu>
2013-05-01 14:17:15 -0700

    Fixed the stubs in the code and made sure the tests passed.  Also finished the Javadoc for the file.

--- src/edu/ucsb/cs56/S13/lab04/iDarvis/Car.java
+++ src/edu/ucsb/cs56/S13/lab04/iDarvis/Car.java
@@ -1,9 +1,17 @@
 package edu.ucsb.cs56.S13.lab04.iDarvis;
 
+/** Car class that I have come up with.  This class is able to store the Brand's 
+ * of various cars and the year that that car was Manufactured.  For example, 
+ * if you had a 1998 Toyota Carolla, what you would store is the car Brand, Toyota, 
+ * and the year that it was manufactured, 1998.
+
+@author Daryl Pham
+@version CS56, S13, lab04
+*/
     public class Car {
 
-    private String CarName = "stub";//"Toyota"; 
-    private int Year= -42;//1998; 
+    private String CarName = "Toyota"; 
+    private int Year= 1998; 
 
     /** Default Constructor creates a Radio Station KCSB, 99.9 */
 
@@ -14,8 +22,8 @@ package edu.ucsb.cs56.S13.lab04.iDarvis;
 	@param Year year  (e.g. 2013, 1998, 2005, etc.)
      */
     public Car(String name, int year) { 
-	this.CarName = "stub";//name;
-	this.Year = -42;//year;
+	this.CarName = name;
+	this.Year = year;
     }
 
     /**
@@ -34,13 +42,13 @@ package edu.ucsb.cs56.S13.lab04.iDarvis;
        @set Car Name  (e.g. Toyota, BMW, Dodge, Ford)       
     */
     public void setCarName(String name) { 
-	this.CarName = "stub";//name; 
+	this.CarName = name; 
     }
      /**
        @set Manufacture year (e.g. 2013, 1998, 2005)
      */
     public void setYear(int year) { 
-	this.Year = -42;//year; 
+	this.Year = year; 
     } 
     
   /*  /**
@@ -56,9 +64,7 @@ isFM() that returns a true or false value. It should return true when frequency 
        @return String represntation of the car.
      */
     public String toString() {
-	/*java.text.DecimalFormat amFormat = new java.text.DecimalFormat("0");
-	java.text.DecimalFormat fmFormat = new java.text.DecimalFormat("0.0");*/
-	return "stub";//"[" + this.CarName + ", Manufactured: " + this.Year + "]";
+	return "[" + this.CarName + ", Manufactured: " + this.Year + "]";
 }
     /** 
 	A sample main that uses the class.
