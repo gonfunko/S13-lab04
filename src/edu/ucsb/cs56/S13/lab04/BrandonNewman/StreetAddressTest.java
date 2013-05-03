8694c0d50e85463cad5605a04b396513ed4cbfa4
Brandon Newman <bnewman@umail.ucsb.edu>
2013-05-02 22:26:37 -0700

    Added javadoc comments

--- src/edu/ucsb/cs56/S13/lab04/BrandonNewman/StreetAddressTest.java
+++ src/edu/ucsb/cs56/S13/lab04/BrandonNewman/StreetAddressTest.java
@@ -6,11 +6,21 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 
+/**
+Test class for StreetAddress
 
+@author Brandon Newman
+@version 2013/05/02 for lab04, cs56, s13
+@see StreetAddress
+*/
 public class StreetAddressTest
 {
 	public static final double TOL = 0.00001;
 
+	/**
+	Test case for the no argument Constructor
+	@see StreetAddress
+	*/
 	@Test public void testNoArgConstructor()
 	{
 		StreetAddress s = new StreetAddress();
@@ -18,6 +28,10 @@ public class StreetAddressTest
 		assertEquals(0, s.getStreetNumber());
 	}
 
+	/**
+	Test case for the two argument Constructor
+	@see StreetAddress
+	*/
 	@Test public void testTwoArgConstructor()
 	{
 		StreetAddress s = new StreetAddress("Pardall", 192);
@@ -25,6 +39,10 @@ public class StreetAddressTest
 		assertEquals(192, s.getStreetNumber());
 	}
 
+	/**
+	Test case for StreetAddress.setStreetName()
+	@see StreetAddress
+	*/
 	@Test public void testSetStreetName()
 	{
 		StreetAddress s = new StreetAddress();
@@ -32,6 +50,10 @@ public class StreetAddressTest
 		assertEquals("Alana Lane", s.getStreetName());
 	}
 
+	/**
+	Test case for StreetAddress.setStreetNumber()
+	@see StreetAddress
+	*/
 	@Test public void testSetStreetNumber()
 	{
 		StreetAddress s = new StreetAddress();
@@ -39,34 +61,46 @@ public class StreetAddressTest
 		assertEquals(544, s.getStreetNumber());
 	}
 
+	/**
+	Test case for StreetAddress.getStreetName()
+	@see StreetAddress
+	*/
 	@Test public void testGetStreetName()
 	{
 		StreetAddress s = new StreetAddress("Camino Del Sur", 785);
 		assertEquals("Camino Del Sur", s.getStreetName());
 	}
 
+	/**
+	Test case for StreetAddress.getStreetNumber()
+	@see StreetAddress
+	*/
 	@Test public void testGetStreetNumber()
 	{
 		StreetAddress s = new StreetAddress("Camino Del Sur", 785);
 		assertEquals(785, s.getStreetNumber());
 	}
 
+	/**
+	Test case for StreetAddress.toString()
+	@see StreetAddress
+	*/
 	@Test public void testToString()
 	{
 		StreetAddress s = new StreetAddress("Lone Oak Drive", 2413);
 		assertEquals("2413 Lone Oak Drive", s.toString());
 	}
 
+	/**
+	Test case for StreetAddress.equals()
+	@see StreetAddress
+	*/
 	@Test public void testEquals()
 	{
 		StreetAddress s = new StreetAddress("Lone Oak Drive", 2413);
 		StreetAddress s2 = new StreetAddress("Lone Oak Drive", 2413);
 		assertEquals(true, s.equals(s2));
 	}
-
-
-
-
 
 
 }
