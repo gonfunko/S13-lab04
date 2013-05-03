8db173f34a9ffea4ca4fc573dcc489aac45246fb
Vetebullen <Vetebullen@gmail.com>
2013-05-03 13:33:58 -0700

    Methods implemented, all tests pass

--- src/edu/ucsb/cs56/S13/lab04/Vetebullen/Skyscraper.java
+++ src/edu/ucsb/cs56/S13/lab04/Vetebullen/Skyscraper.java
@@ -5,18 +5,40 @@ public class Skyscraper{
 	private String name;
 	public Skyscraper(){
 
+		this.noOfStories = 0;
+		this.name = "";
 	}
 	public Skyscraper(int noOfStories,String name){
-
+		this.noOfStories = noOfStories;
+		this.name = name;
 	}
-	public String getName(){return "stub";}	//TODO stub
-	public int getNoOfStories(){return -1;}//TODO stub
-	public void setName(String name){}//TODO stub
-	public void setNoOfStories(int noOfStories){}//TODO stub
-	public String toString(){return "stub";}//TODO stub
-	public boolean equals(Object o){return false;}//TODO stub
+	public String getName(){return this.name;}
+	public int getNoOfStories(){return this.noOfStories;}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setNoOfStories(int noOfStories){
+		this.noOfStories = noOfStories;
+	}
+	public String toString(){return this.name+" has "+this.noOfStories+" stories";}
+	public boolean equals(Object o){
+		if(o==null){
+			return false;
+		}
+		if(!(o instanceof Skyscraper)){
+			return false;
+		}
+		Skyscraper s = (Skyscraper) o;
+
+		if(this.getName().equals(s.getName())&&
+		this.getNoOfStories()==s.getNoOfStories()){
+			return true;	
+		}
+		return false;
+	}
 	
 	public static void main(String[] args){
-		//TODO stub
+		Skyscraper s = new Skyscraper(102,"Empire State Building");
+		s.toString();
 	}
 }
