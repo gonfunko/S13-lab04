575beb0798d553513c342c2ea2ea94bcd083bea1
Vetebullen <Vetebullen@gmail.com>
2013-05-03 13:28:16 -0700

    Stubs and tests finished, all tests fail

---
+++ src/edu/ucsb/cs56/S13/lab04/Vetebullen/Skyscraper.java
@@ -0,0 +1,22 @@
+package edu.ucsb.cs56.S13.lab04.Vetebullen;
+
+public class Skyscraper{
+	private int noOfStories;
+	private String name;
+	public Skyscraper(){
+
+	}
+	public Skyscraper(int noOfStories,String name){
+
+	}
+	public String getName(){return "stub";}	//TODO stub
+	public int getNoOfStories(){return -1;}//TODO stub
+	public void setName(String name){}//TODO stub
+	public void setNoOfStories(int noOfStories){}//TODO stub
+	public String toString(){return "stub";}//TODO stub
+	public boolean equals(Object o){return false;}//TODO stub
+	
+	public static void main(String[] args){
+		//TODO stub
+	}
+}
